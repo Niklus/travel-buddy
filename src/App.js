59cfc9cb03fd54b18b3beda2db4c75c10f8052d7
Nicholas Otieno <niklasoti@gmail.com>
2021-03-19 02:29:16 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useLayoutEffect } from "react";
+import dayjs from "dayjs";
 import Http from "./utils/http";
 import mapboxgl from "mapbox-gl";
+import countries from "./countries";
 
 const http = new Http();
 
@@ -46,10 +48,20 @@ function App() {
       .get(`/api/timezone/${country.timezone.name}`)
       .then((data) => {
         setTime(data);
+        console.log(data);
       })
       .catch((err) => {
         console.error("OOPS:" + err);
       });
+  }
+
+  function handleNameChange(event) {
+    const value = event.target.value;
+    const newValue = value.charAt(0).toUpperCase() + value.slice(1);
+
+    if (countries.indexOf(newValue) !== -1) {
+      getCountry(newValue);
+    }
   }
 
   if (Object.keys(country).length === 0) {
@@ -61,6 +73,20 @@ function App() {
       <header>
         <h1>Travelbuddy</h1>
       </header>
+      <div className="datalist">
+        <input
+          list="countries"
+          id="myCountry"
+          name="myCountry"
+          placeholder="Search for a country"
+          onChange={handleNameChange}
+        />
+        <datalist id="countries">
+          {countries.map((country) => {
+            return <option key={country} value={`${country}`} />;
+          })}
+        </datalist>
+      </div>
       <main>
         <div className="info">
           <section className="name">
@@ -71,11 +97,13 @@ function App() {
             />
             <div>
               <h1>{country.names.name}</h1>
-              <h1>{country.names.full}</h1>
+              <h3>{country.names.full}</h3>
             </div>
           </section>
           <section>
-            <h2>Travel Advice</h2>
+            <h2>
+              Travel Advice <span className="material-icons">flight</span>
+            </h2>
             <hr />
             <p>{country.advise.UA.advise}</p>
             <p
@@ -83,7 +111,9 @@ function App() {
             ></p>
           </section>
           <section>
-            <h2>Drinking Water</h2>
+            <h2>
+              Drinking Water <span className="material-icons">local_drink</span>
+            </h2>
             <hr />
             <p>
               Drinking tap water in {country.names.name} is{" "}
@@ -91,7 +121,9 @@ function App() {
             </p>
           </section>
           <section>
-            <h2>Language</h2>
+            <h2>
+              Language <span className="material-icons">language</span>
+            </h2>
             <hr />
             <p>
               The language(s) spoken in {country.names.name} is{" "}
@@ -100,21 +132,33 @@ function App() {
             </p>
           </section>
           <section>
-            <h2>Time Zone</h2>
+            <h2>
+              Time Zone <span className="material-icons">schedule</span>
+            </h2>
             <hr />
             <p>{country.timezone.name}</p>
-            <p>{time.datetime}</p>
+            <p>
+              {dayjs(time.datetime).format("MMM D YYYY")}
+              <span> {time.abbreviation}</span>
+            </p>
           </section>
           <section>
-            <h2>Currency</h2>
+            <h2>
+              Currency <span className="material-icons">account_balance</span>
+            </h2>
             <hr />
             <p>
               The currency in {country.names.name} is {country.currency.name} -{" "}
               {country.currency.code}
             </p>
           </section>
+        </div>
+        <div>
+          <div id="map" style={{ width: "470px", height: "500px" }}></div>
           <section>
-            <h2>Telephone</h2>
+            <h2>
+              Telephone <span className="material-icons">call_end</span>
+            </h2>
             <hr />
             <p style={{ lineHeight: "30px" }}>
               Calling Code: +{country.telephone.calling_code}
@@ -126,10 +170,17 @@ function App() {
               Fire: {country.telephone.fire}
             </p>
           </section>
+          <section>
+            <h2>
+              Electricity <span className="material-icons">bolt</span>
+            </h2>
+            <hr />
+            <p>Voltage: {country.electricity.voltage} Volt</p>
+            <p>Frequency: {country.electricity.frequency} Herz</p>
+          </section>
         </div>
-        <div id="map" style={{ width: "450px", height: "500px" }}></div>
       </main>
-      <pre>{JSON.stringify(country, null, 3)}</pre>
+      {/*<pre>{JSON.stringify(country, null, 3)}</pre>*/}
     </>
   );
 }
